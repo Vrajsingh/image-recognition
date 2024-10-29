0ad98e61b15f62c76fe625e226c6b7bfbf2ab881
Vrajsingh <vishalrajput97084@gmail.com>
2024-10-29 15:16:37 +0530

    Initial commit

---
+++ src/test/java/com/ai/image_recognition/ImageRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.image_recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
